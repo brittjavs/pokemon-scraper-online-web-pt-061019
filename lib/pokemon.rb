aa0f6cd5871f65b2e838c7c8d8753221663b7ce8
Brittany Javalera <brittjavalera@gmail.com>
2019-08-20 16:36:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,16 +17,16 @@ attr_accessor :id, :name, :type, :db
   # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   
-  def self.find(id, db)
-    sql = <<-SQL
-    SELECT * FROM pokemon WHERE id = ?;
-    SQL
-    db.execute(sql, id).map do |row|
-      name = row[1]
-      type = row[2]
-      new_pokemon = self.new(id, name, type)
-      new_pokemon
-    end
-  end
+  # def self.find(id, db)
+  #   sql = <<-SQL
+  #   SELECT * FROM pokemon WHERE id = ?;
+  #   SQL
+  #   db.execute(sql, id).map do |row|
+  #     name = row[1]
+  #     type = row[2]
+  #     new_pokemon = self.new(id, name, type)
+  #     new_pokemon
+  #   end
+  # end
   
 end
