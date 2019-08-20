411ce5522c35a8a466a494341eb915dc0633ae46
Brittany Javalera <brittjavalera@gmail.com>
2019-08-20 15:59:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
 class Pokemon
+attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+  @id = id
+  @name = name
+  @type = type
+  @db = db
+  end
+  
+  def self.save
+  sql = <<-SQL
+  INSERT INTO pokemon(name, type)
+  VALUES (?, ?)
+  SQL
+  DB[:conn].execute(sql)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+  
+  def self.find(id)
+    sql = <<-SQL
+    SELECT * FROM pokemon WHERE id = ? LIMIT 1
+    SQL
+    DB[:CONN].execute(sql, self.id, self.db).map do |row|
+      id = row[0]
+      name = row[1]
+      type = row[2]
+      self.new(id, name, type)
+    end
+  end
+  
 end
