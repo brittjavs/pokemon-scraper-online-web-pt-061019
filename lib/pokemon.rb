6631faccb3c726052ea043eddee2411bf8f290bb
Brittany Javalera <brittjavalera@gmail.com>
2019-08-20 16:08:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,22 +5,23 @@ attr_accessor :id, :name, :type, :db
   @id = id
   @name = name
   @type = type
+  @db = db
   end
   
-  def self.save(name, type)
+  def self.save(name, type, db)
   sql = <<-SQL
   INSERT INTO pokemon(name, type)
   VALUES (?, ?)
   SQL
   DB[:conn].execute(sql)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   
-  def self.find(number)
+  def self.find(number, db)
     sql = <<-SQL
-    SELECT * FROM pokemon WHERE id = number LIMIT 1
+    SELECT * FROM pokemon WHERE id = ? LIMIT 1
     SQL
-    DB[:CONN].execute(sql, self.id).map do |row|
+    DB[:CONN].execute(sql, self.id, self.db).map do |row|
       id = row[0]
       name = row[1]
       type = row[2]
